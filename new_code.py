2874b7584b4bd48b5ec6e0105f9e4c16023122dc
rpeseng <rojhan2147@gmail.com>
2024-02-10 22:13:43 +0300

    This projets try raspberyy pi.

--- new_code.py
+++ new_code.py
@@ -12,47 +12,57 @@ set_hum_min = 65
 set_hum_max = 75
 
 
-# GPIO pin numaraları
-SET_PIN = 19
-INCREASE_PIN = 18
-DECREASE_PIN = 16
+class ButtonController:
+    def __init__(self, set_pin, increase_pin, decrease_pin):
+        self.set_button = Button(set_pin)
+        self.increase_button = Button(increase_pin)
+        self.decrease_button = Button(decrease_pin)
 
-# Butonlar
-set_button = Button(SET_PIN)
-increase_button = Button(INCREASE_PIN)
-decrease_button = Button(DECREASE_PIN)
+        self.counter = 0
+
+        # Buton tetikleyicileri atanıyor
+        self.set_button.when_pressed = self.set_pressed
+        self.increase_button.when_pressed = self.increase_pressed
+        self.decrease_button.when_pressed = self.decrease_pressed
+
+        # Butonlar için debouncing süresi
+        self.debounce_time = 0.1
+        self.last_time_set = time.time()
+        self.last_time_increase = time.time()
+        self.last_time_decrease = time.time()
+
+    def set_pressed(self):
+        current_time = time.time()
+        if current_time - self.last_time_set > self.debounce_time:
+            self.counter = 0
+            print("Set button pressed")
+            self.last_time_set = current_time
+
+    def increase_pressed(self):
+        current_time = time.time()
+        if current_time - self.last_time_increase > self.debounce_time:
+            self.counter += 1
+            print("Increase button pressed")
+            self.last_time_increase = current_time
+
+    def decrease_pressed(self):
+        current_time = time.time()
+        if current_time - self.last_time_decrease > self.debounce_time:
+            self.counter -= 1
+            print("Decrease button pressed")
+            self.last_time_decrease = current_time
+
+    def run(self):
+        try:
+            while True:
+                print("Counter:", self.counter)
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print("Program sonlandırılıyor...")
 
 
-# Değişkenler
-counter = 0
+# ButonController sınıfını kullanarak nesne oluştur
+button_controller = ButtonController(set_pin=17, increase_pin=18, decrease_pin=19)
 
-# Tuş işlevleri
-def set_pressed():
-    global counter
-    print("Set button pressed")
-    counter = 0  # Sıfırla
-
-def increase_pressed():
-    global counter
-    print("Increase button pressed")
-    counter += 1  # Artır
-
-def decrease_pressed():
-    global counter
-    print("Decrease button pressed")
-    counter -= 1  # Azalt
-
-# Buton tetikleyicileri
-set_button.when_pressed = set_pressed
-increase_button.when_pressed = increase_pressed
-decrease_button.when_pressed = decrease_pressed
-
-try:
-    while True:
-        # Her saniyede bir sayaç değerini göster
-        print("Counter:", counter)
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("Program sonlandırılıyor...")
-finally:
-    pass  # GPIO temizleme kodu buraya eklenecek
+# Ana döngüyü başlat
+button_controller.run()
