d5015d54a423351d083d75a1eb5af53330704cd7
rpeseng <rojhan2147@gmail.com>
2024-02-11 01:22:03 +0300

    This projets try raspberyy pi.

--- new_code.py
+++ new_code.py
@@ -80,7 +80,6 @@ class ButtonController:
             elif selected_item == "set_hum_min":
                 self.set_hum_min_function()
             elif selected_item == "set_hum_max":
-                self.count = 0
                 self.set_hum_max_function()
         """
         print("Set button pressed")
