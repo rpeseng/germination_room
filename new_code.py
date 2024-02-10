70325870f23375a3ab6fcda73a39d5da6cd178d4
rpeseng <rojhan2147@gmail.com>
2024-02-11 01:24:49 +0300

    This projets try raspberyy pi.

--- new_code.py
+++ new_code.py
@@ -148,8 +148,11 @@ class ButtonController:
                                 r = i - 2
                                 self.lcd.write("> ")
                                 self.lcd.write(self.items[i])
-                print(set_temp_min)
-                time.sleep(0.2)
+                    time.sleep(0.2)
+
+                else:
+                    print(set_temp_min)
+                    time.sleep(0.2)
         except KeyboardInterrupt:
             self.lcd.clear_screen()
             self.lcd.print_on_lcd("LCD Deactive", 1)
