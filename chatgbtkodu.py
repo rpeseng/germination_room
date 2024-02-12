cc2297383d0b658e9e16c83a2e33095a027a6b3a
rpeseng <rojhan2147@gmail.com>
2024-02-13 00:48:57 +0300

    This projets try raspberyy pi.

--- chatgbtkodu.py
+++ chatgbtkodu.py
@@ -1,101 +1,76 @@
-from RPLCD.i2c import CharLCD
 import RPi.GPIO as GPIO
-import time
+from RPLCD.i2c import CharLCD
+from time import sleep
 
-# GPIO pin tanımları
-SET_PIN = 18
-INCREASE_PIN = 16
-DECREASE_PIN = 26
-ENTER_PIN = 4
-
-# LCD'nin I2C adresi
-lcd = CharLCD(i2c_expander='PCF8574', address=0x27,port=1, cols=20, rows=4)
-
-# Menü seçenekleri
-menu_items = ["set_temp_min", "set_temp_max", "set_hum_min", "set_hum_max"]
+# LCD ekranı için pin ayarları
+lcd = CharLCD('PCF8574', 0x27)
 
 # Buton pinlerini tanımla
-button_pins = [SET_PIN, INCREASE_PIN, DECREASE_PIN, ENTER_PIN]
+BUTTON_PIN_1 = 17
+BUTTON_PIN_2 = 18
+BUTTON_PIN_3 = 27
 
-# Butonların GPIO tanımlarını ayarla
+# GPIO pinlerini ayarla
 GPIO.setmode(GPIO.BCM)
-for pin in button_pins:
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON_PIN_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON_PIN_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON_PIN_3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Menüyü tutacak sınıf
-class MenuOption:
-    def __init__(self, name, next_menu=None, prev_menu=None, child_menu=None, parent_menu=None, menu_function=None):
-        self.name = name
-        self.next = next_menu
-        self.prev = prev_menu
-        self.child = child_menu
-        self.parent = parent_menu
-        self.menu_function = menu_function
+# Başlangıç değerleri
+menu_items = ["Setting", "About", "Exit"]
+submenu_items = ["Set Hum Value", "Set Temp Value", "Back"]
+selected_menu_item = 0
+selected_submenu_item = 0
+humidity_value = 0
+temperature_value = 0
 
-# Menü öğelerini oluştur
-
-menu2 = MenuOption("DATE AND TIME")
-menu3 = MenuOption("LOGGING")
-menu4 = MenuOption("OUTPUT")
-menu5 = MenuOption("SETTINGS")
-# Alt menü öğelerini oluştur
-sub_menu1_1 = MenuOption("Offset")
-sub_menu1_2 = MenuOption("Datum")
-
-# Ana menü öğelerini oluştur
-menu1 = MenuOption("CONFIGRATION", next_menu=sub_menu1_2, prev_menu=sub_menu1_1)
-
-
-menu1.next = menu2
-menu1.prev = menu5
-
-menu2.next = menu3
-menu2.prev = menu1
-
-menu3.next = menu4
-menu3.prev = menu2
-
-menu4.next = menu5
-menu4.prev = menu3
-
-menu5.next = menu1
-menu5.prev = menu4
-
-current_menu = menu1
-
-# LCD'yi temizleme fonksiyonu
-def clear_lcd():
+def display_menu():
     lcd.clear()
+    lcd.cursor_pos = (0, 0)
+    lcd.write_string(menu_items[selected_menu_item])
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string(submenu_items[selected_submenu_item])
 
-# LCD'ye mesaj yazma fonksiyonu
-def write_lcd(message):
+def update_submenu():
     lcd.clear()
-    lcd.write_string(message)
+    lcd.cursor_pos = (0, 0)
+    lcd.write_string(submenu_items[selected_submenu_item])
 
-# Ana döngü
+def increase_value():
+    global humidity_value, temperature_value
+    if selected_submenu_item == 0:
+        humidity_value += 1
+    elif selected_submenu_item == 1:
+        temperature_value += 1
+
+def decrease_value():
+    global humidity_value, temperature_value
+    if selected_submenu_item == 0:
+        humidity_value -= 1
+    elif selected_submenu_item == 1:
+        temperature_value -= 1
+
 try:
     while True:
-        # Ana menüyü ekrana yazdır
-        write_lcd("MAIN MENU\n")
-        write_lcd("> " + current_menu.name)
-
-        # Butonları kontrol et
-        for pin in button_pins:
-            if not GPIO.input(pin):
-                if pin == SET_PIN:
-                    if current_menu.menu_function:
-                        current_menu.menu_function()
-                    if current_menu.child:
-                        current_menu = current_menu.child
-                elif pin == INCREASE_PIN:
-                    current_menu = current_menu.next
-                elif pin == DECREASE_PIN:
-                    current_menu = current_menu.prev
-                elif pin == ENTER_PIN:
-                    if current_menu.parent:
-                        current_menu = current_menu.parent
-
-        time.sleep(0.1)
+        if GPIO.input(BUTTON_PIN_1) == GPIO.LOW:
+            selected_menu_item = (selected_menu_item + 1) % len(menu_items)
+            display_menu()
+            sleep(0.2)
+        elif GPIO.input(BUTTON_PIN_2) == GPIO.LOW:
+            if selected_menu_item == 0:  # Setting
+                selected_submenu_item = (selected_submenu_item + 1) % len(submenu_items)
+                update_submenu()
+            elif selected_menu_item == 2:  # Exit
+                break
+            sleep(0.2)
+        elif GPIO.input(BUTTON_PIN_3) == GPIO.LOW:
+            if selected_menu_item == 0 and selected_submenu_item == 2:  # Back from Setting submenu
+                selected_submenu_item = 0
+                display_menu()
+            sleep(0.2)
 
 except KeyboardInterrupt:
+    pass
+
+finally:
     GPIO.cleanup()
