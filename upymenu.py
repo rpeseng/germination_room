48f7820e3eb31a4547078c8607a72576b5f0b64a
rpeseng <rojhan2147@gmail.com>
2024-02-15 22:32:46 +0300

    This projets try raspberyy pi.

--- upymenu.py
+++ upymenu.py
@@ -1,4 +1,98 @@
-from lib.upymenu import Menu, MenuAction, MenuNoop
+import RPi.GPIO as GPIO
+from RPLCD import i2c
+
+# Buton pinleri
+buton_yukari = 23
+buton_asagi = 24
+buton_secim = 25
+
+# LCD ekran pinleri
+lcd_i2c_adres = 0x27
+lcd_kolonlar = 16
+lcd_satirlar = 2
+
+
+
+# Fonksiyonlar
+def fonksiyon1():
+    # ...
+
+def fonksiyon2():
+    # ...
+
+def fonksiyon3():
+    # ...
+
+def alt_fonksiyon1():
+    # ...
+
+def alt_fonksiyon2():
+    # ...
+
+def alt_fonksiyon3():
+    # ...
+
+
+    # Menü ve submenüler
+menu = {
+    "Ana Menü": {
+        "1. Fonksiyon": fonksiyon1,
+        "2. Fonksiyon": fonksiyon2,
+        "3. Fonksiyon": fonksiyon3
+    },
+    "1. Fonksiyon Alt Menüsü": {
+        "1.1. Alt Fonksiyon": alt_fonksiyon1,
+        "1.2. Alt Fonksiyon": alt_fonksiyon2
+    },
+    "2. Fonksiyon Alt Menüsü": {
+        "2.1. Alt Fonksiyon": alt_fonksiyon3
+    }
+}
+
+# LCD ekranı başlat
+lcd = i2c(lcd_i2c_adres, lcd_kolonlar, lcd_satirlar)
+
+# Butonları ayarla
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buton_yukari, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(buton_asagi, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(buton_secim, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Mevcut menü ve seçilen öğeyi takip et
+mevcut_menu = "Ana Menü"
+secilen_oge = 0
+
+# Ana döngü
+while True:
+    # Buton basışlarını kontrol et
+    if GPIO.input(buton_yukari) == GPIO.LOW:
+        secilen_oge -= 1
+    elif GPIO.input(buton_asagi) == GPIO.LOW:
+        secilen_oge += 1
+    elif GPIO.input(buton_secim) == GPIO.LOW:
+        # Seçilen öğeyi çalıştır
+        if mevcut_menu in menu and secilen_oge in menu[mevcut_menu]:
+            menu[mevcut_menu][list(menu[mevcut_menu].keys())[secilen_oge]]()
+
+    # Ekranı güncelle
+    lcd.clear()
+    lcd.cursor_pos = (0, 0)
+    lcd.write(mevcut_menu)
+    for i, oge in enumerate(menu[mevcut_menu]):
+        lcd.cursor_pos = (1, i)
+        if i == secilen_oge:
+            lcd.write("> " + oge)
+        else:
+            lcd.write("  " + oge)
+
+    # Seçilen öğenin sınırlarını kontrol et
+    if secilen_oge < 0:
+        secilen_oge = len(menu[mevcut_menu]) - 1
+    elif secilen_oge >= len(menu[mevcut_menu]):
+        secilen_oge = 0
+
+
+"""from lib.upymenu import Menu, MenuAction, MenuNoop
 from data.lcd_library import LCDController
 from RPLCD import *
 from RPLCD.i2c import CharLCD
@@ -27,4 +121,4 @@ menu.focus_prev()
 
 menu = menu.choose()
 
-menu = menu.parent()
+menu = menu.parent()"""
